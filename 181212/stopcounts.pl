f855e3c125c4b353069a34ae5f418b7257e544c2
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2018-12-15 17:12:52 +0800

    Update stopcounts.pl

--- 181212/stopcounts.pl
+++ 181212/stopcounts.pl
@@ -3,48 +3,40 @@ use strict;
 
 open IN1,"<", $ARGV[0];
 open IN2,"<", $ARGV[1];
-my $OUT = "nor.$ARGV[0]";
-open OUT1,">", $OUT;
-open OUT2,">", $ARGV[2];
+open OUT,">", $ARGV[2];
 
-my $map_len = $ARGV[3];
-
+my $map_len = 18;
 my %bas;
-while(<IN1>){
+chomp(my $raw = <IN1>);
+
+while(<IN2>){
 	chomp;
 	my @tmp = split /\t/, $_;
 	my @ls = split //, $tmp[2];
 	my $base = join('',@ls[-$map_len..-1]);
-	if (exists $bas{$base}){
-		$bas{$base} += $tmp[1];
-	}else{
-		$bas{$base} = $tmp[1];
+	if ($raw =~ /$tmp[2]/){
+		if (exists $bas{$base}){
+			$bas{$base} += $tmp[1];
+		}else{
+			$bas{$base} = $tmp[1];
+		}
 	}
 }
 
 
-foreach my $char (sort { $bas{$b} <=> $bas{$a} } keys %bas)
-{
-    print OUT1 "$char\t$bas{$char}\n";
-}
-
-
-my $raw = <IN2>;
-chomp($raw);
 my @refbas = split //, $raw;
 my $len = $#refbas;
 foreach my $i (0..$len-$map_len+1) {
 	my @tmp = @refbas[$i..$i+$map_len-1];
 	my $barc = join('',@tmp);
 	if (exists $bas{$barc}){
-                print OUT2 "$barc\t$bas{$barc}\r\n";
+                print OUT "$barc\t$bas{$barc}\r\n";
         }else{
-                print OUT2 "$barc\tNo Match.\r\n";
+                print OUT "$barc\tNo Match.\r\n";
         }
 }
 
 
 close IN1;
 close IN2;
-close OUT1;
-close OUT2;
+close OUT;
