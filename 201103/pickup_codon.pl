60f8a3368cc849962022e1fb3f1a62a679ff7b90
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-11-03 12:35:00 +0800

    201103

---
+++ 201103/pickup_codon.pl
@@ -0,0 +1,62 @@
+use strict;
+use warnings;
+use autodie;
+
+use Getopt::Long;
+use AlignDB::IntSpan;
+
+sub GET_ID {
+    my ( $FEATURE, $INFO ) = @_;
+    my $ID;
+    if ( $INFO =~ m/$FEATURE(\w+[0-9]*)/ ) {
+        $ID = $1;
+    }
+    else {
+        warn("There is a problem in $INFO;\n");
+    }
+    return $ID;
+}
+
+my ( %trans, %gene, %no, %start_codon, %chr_dir, %exon, %intron, %mrna );
+
+open( my $IN_FH1, "<", $ARGV[0] );
+while (<$IN_FH1>) {
+    chomp;
+    my ( $trans, $gene, $chr, $dir, $start, $end, $no ) = split "\t";
+    $trans{$trans}       = 1;
+    $gene{$trans}        = $gene;
+    $start_codon{$trans} = "$start-$end";
+    $chr_dir{$trans}     = "$chr\t$dir";
+    $no{$trans}          = $no;
+}
+close($IN_FH1);
+
+open( my $IN_FH2, "<", $ARGV[1] );
+while (<$IN_FH2>) {
+    chomp;
+    my ( undef, undef, $type, $start, $end, undef, undef, undef, $info ) =
+      split /\t/;
+    my $trans_id = GET_ID( "transcript_id=", $info );
+    if ( exists( $trans{$trans_id} ) ) {
+        if ( $type eq "transcript" ) {
+            $mrna{$trans_id} = AlignDB::IntSpan->new;
+            $mrna{$trans_id}->AlignDB::IntSpan::add_range( $start, $end );
+        }
+        elsif ( $type eq "exon" ) {
+            if ( exists( $exon{$trans_id} ) ) {
+                $exon{$trans_id}->AlignDB::IntSpan::add_range( $start, $end );
+            }
+            else {
+                $exon{$trans_id} = AlignDB::IntSpan->new;
+                $exon{$trans_id}->AlignDB::IntSpan::add_range( $start, $end );
+            }
+        }
+    }
+}
+close($IN_FH2);
+
+foreach my $trans ( keys(%trans) ) {
+    $intron{$trans} = $mrna{$trans}->AlignDB::IntSpan::diff( $exon{$trans} );
+    print
+"$trans\t$gene{$trans}\t$no{$trans}\t$mrna{$trans}\t$exon{$trans}\t$intron{$trans}\t$start_codon{$trans}\n";
+}
