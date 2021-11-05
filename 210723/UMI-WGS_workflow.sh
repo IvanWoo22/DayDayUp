6c9db89b734cc3b332b81eaf74a4cab9228b6489
Ivan <wuyifanwd@hotmail.com>
2021-11-05 19:12:25 +0800

    211105.

--- 210723/UMI-WGS_workflow.sh
+++ 210723/UMI-WGS_workflow.sh
@@ -1,8 +1,24 @@
 umi_tools extract \
   --bc-pattern=NNNNNN --bc-pattern2=NNNNNN \
-  -I cater_data/NJU9221/NJU9221_R1.fq.gz \
-  -S processed.1.fastq.gz \
-  --read2-in=cater_data/NJU9221/NJU9221_R2.fq.gz \
-  --read2-out=processed.2.fastq.gz
+  -I cater_data/NJU9233/NJU9233_R1.fq.gz \
+  -S fat/NJU9233/processed.1.fastq.gz \
+  --read2-in=cater_data/NJU9233/NJU9233_R2.fq.gz \
+  --read2-out=fat/NJU9233/processed.2.fastq.gz
 
-umi_tools dedup -I bwa_sorted.bam --paired -S bwa_deduplicated.bam --output-stats=deduplicated
+time bwa-mem2 mem -t 12 \
+  fat/index/hg38_genome.fa.gz \
+  fat/NJU9233/processed.1.fastq.gz \
+  fat/NJU9233/processed.2.fastq.gz \
+  1>fat/NJU9233/bwa_raw.sam \
+  2>fat/NJU9233/bwa.log
+
+samtools sort -@ 8 \
+  fat/NJU9233/bwa_raw.sam \
+  -o fat/NJU9233/bwa_sorted.bam
+
+samtools index fat/NJU9233/bwa_sorted.bam
+
+umi_tools dedup \
+  --paired --output-stats=deduplicated \
+  -I fat/NJU9233/bwa_sorted.bam \
+  -S fat/NJU9233/bwa_deduplicated.bam
