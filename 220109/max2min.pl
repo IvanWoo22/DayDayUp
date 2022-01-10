5b85d7069d43f84d9dfe33a08fed9fa10e756c03
Ivan <wuyifanwd@hotmail.com>
2022-01-10 19:17:31 +0800

    220110.

--- 220109/max2min.pl
+++ 220109/max2min.pl
@@ -10,24 +10,24 @@ open( my $TSV, "<", $ARGV[0] );
 
 readline($TSV);
 my @dif;
-while(<$TSV>){
+while (<$TSV>) {
     chomp;
-    my @tmp  = split /\t/;
+    my @tmp = split /\t/;
     my @val_ch;
-    foreach my $i (1..$#tmp){
-        if ($tmp[$i] ne "NA") {
-            push(@val_ch,$tmp[$i]);
+    foreach my $i ( 1 .. $#tmp ) {
+        if ( $tmp[$i] ne "NA" ) {
+            push( @val_ch, $tmp[$i] );
         }
     }
-    if ($#val_ch > 2) {
-        push(@dif,max(@val_ch)-min(@val_ch));
+    if ( $#val_ch > 2 ) {
+        push( @dif, max(@val_ch) - min(@val_ch) );
     }
 }
 
 close($TSV);
 
 my $median = Statistics::Basic::median(@dif);
-my $std = Statistics::Basic::stddev(@dif);
+my $std    = Statistics::Basic::stddev(@dif);
 print "$#dif\t$median\t$std\n";
 
 __END__
