496c211c5185e02fed0ced6db7e24f83d22b353d
Ivan <wuyifanwd@hotmail.com>
2022-03-09 10:33:19 +0800

    220309.

--- 220308/fetch_transcript4validation.pl
+++ 220308/fetch_transcript4validation.pl
@@ -3,8 +3,6 @@ use strict;
 use warnings;
 use autodie;
 
-my %sequences;
-
 sub SEQ_TR_TU {
     my $SEQ = shift;
     return ( $SEQ =~ tr/Uu/Tt/r );
@@ -12,6 +10,7 @@ sub SEQ_TR_TU {
 
 open my $FA, "<", $ARGV[0];
 my $id;
+my %sequences;
 while ( my $line = <$FA> ) {
     chomp $line;
     if ( $line =~ /^(>.*)$/ ) {
@@ -22,6 +21,16 @@ while ( my $line = <$FA> ) {
     }
 }
 close($FA);
+
+open my $TPM, "<", $ARGV[1];
+my %tpm;
+while ( my $line = <$TPM> ) {
+    chomp $line;
+    my ( $raw_id, $tpm ) = split( "\t", $line );
+    my @tmp = split( /\./, $raw_id );
+    $tpm{ $tmp[0] } = $tpm;
+}
+close($TPM);
 
 sub FETCH {
     my $KEY = shift;
@@ -35,27 +44,38 @@ sub FETCH {
     return ( $FETCH_TITLE, $sequences{$FETCH_TITLE} );
 }
 
-open my $LS, "<", $ARGV[1];
+open my $LS, "<", $ARGV[2];
 my $site = 0;
 while ( my $line = <$LS> ) {
     chomp $line;
     $site++;
     my @tmp = split( "\t", $line );
     $tmp[1] = SEQ_TR_TU( $tmp[1] );
+    $tmp[2] = SEQ_TR_TU( $tmp[2] );
     if ( split( /\//, $tmp[0] ) > 1 ) {
         my $opt = 0;
         foreach my $key ( split( /\//, $tmp[0] ) ) {
             $opt++;
             my ( $title, $seq ) = FETCH($key);
-            open my $OUT, ">", $ARGV[2] . "/site_" . $site . "_" . $opt . ".fa";
-            print $OUT ("$title$tmp[1]|$tmp[2]$tmp[3]$tmp[4]\n$seq\n");
+            open my $OUT, ">", $ARGV[3] . "/site_" . $site . "_" . $opt . ".fa";
+            unless ( exists( $tpm{ $tmp[0] } ) ) {
+                $tpm{ $tmp[0] } = "NA";
+            }
+            print $OUT (
+                "$title$tmp[3]$tmp[4]$tmp[5]|$tmp[1]|$tmp[2]|$tpm{$tmp[0]}\n$seq\n"
+            );
             close($OUT);
         }
     }
     else {
         my ( $title, $seq ) = FETCH( $tmp[0] );
-        open my $OUT, ">", $ARGV[2] . "/site_" . $site . ".fa";
-        print $OUT ("$title$tmp[1]|$tmp[2]$tmp[3]$tmp[4]\n$seq\n");
+        open my $OUT, ">", $ARGV[3] . "/site_" . $site . ".fa";
+        unless ( exists( $tpm{ $tmp[0] } ) ) {
+            $tpm{ $tmp[0] } = "NA";
+        }
+        print $OUT (
+            "$title$tmp[3]$tmp[4]$tmp[5]|$tmp[1]|$tmp[2]|$tpm{$tmp[0]}\n$seq\n"
+        );
         close($OUT);
     }
 }
