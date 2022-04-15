6e0c61972e536aa0f1de255f25e64f34970b3654
Ivan <wuyifanwd@hotmail.com>
2022-04-15 17:35:42 +0800

    220415.

--- 220308/RoseTta_manual.sh
+++ 220308/RoseTta_manual.sh
@@ -18,19 +18,10 @@ for i in mcr mgr-d mgr-pl mgr-s mgr-ul mgr mhr mpr uur mgr-p; do
   bsub -n 24 -J "${i}_5" "bash ../step5.sh ${i}.fa ${i}"
 done
 
-bsub -n 24 -J "RosettaTR" '
-source /share/home/wangq/miniconda3/etc/profile.d/conda.sh
-conda activate folding
-parallel -j 16 \
-< /share/home/wangq/wyf/RoseTTAFold/example/MgR_mut/parallel.fold.list \
-> /share/home/wangq/wyf/RoseTTAFold/example/MgR_mut/log/folding.stdout \
-2> /share/home/wangq/wyf/RoseTTAFold/example/MgR_mut/log/folding.stderr
-'
-
 for i in mcr mgr-d mgr-pl mgr-s mgr-ul mgr mhr mpr uur mgr-p; do
-  bsub -n 24 -J "${i}_6" 'bash ../step6.sh ${i}.fa ${i}'
+  bsub -n 24 -J "${i}_6" "bash ../step6.sh ${i}.fa ${i}"
 done
 
 for i in mcr mgr-d mgr-pl mgr-s mgr-ul mgr mhr mpr uur mgr-p; do
-  bsub -n 24 -J "${i}_7" 'bash ../step7.sh ${i}.fa ${i}'
+  bsub -n 24 -J "${i}_7" "bash ../step7.sh ${i}.fa ${i}"
 done
