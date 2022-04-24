421675f8fcd756e0de5b2812da8369c4d24eaae8
Ivan <wuyifanwd@hotmail.com>
2022-04-24 21:48:45 +0800

    220424.

---
+++ 220421/tsv_join.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use autodie;
+
+my %table1;
+my %table2;
+my $col_num1;
+my $col_num2;
+
+open( my $TSV1, "<", $ARGV[0] );
+foreach (<$TSV1>) {
+    chomp;
+    my @tbl  = split /\s+/;
+    my $name = "$tbl[0]\t$tbl[1]";
+    $col_num1 = $#tbl;
+    $table1{$name} = join( "\t", @tbl[ 2 .. $col_num1 ] );
+}
+close($TSV1);
+
+foreach my $FILE_INDEX ( 1 .. $#ARGV ) {
+    open( my $TSV2, "<", $ARGV[$FILE_INDEX] );
+    while (<$TSV2>) {
+        chomp;
+        my @tbl  = split /\s+/;
+        my $name = "$tbl[0]\t$tbl[1]";
+        $col_num2 = $#tbl;
+        $table2{$name} = join( "\t", @tbl[ 2 .. $col_num2 ] );
+    }
+    close($TSV2);
+    my %count;
+    my @uniq = grep { ++$count{$_} < 2 } ( keys(%table1), keys(%table2) );
+    foreach my $key (@uniq) {
+        if ( ( exists( $table1{$key} ) ) and ( exists( $table2{$key} ) ) ) {
+            $table1{$key} .= "\t" . $table2{$key};
+        }
+        elsif ( exists( $table2{$key} ) ) {
+            $table1{$key} =
+              join( "\t", ("NA") x ( $col_num1 - 1 ) ) . "\t" . $table2{$key};
+        }
+        elsif ( exists( $table1{$key} ) ) {
+            $table1{$key} .= "\t" . join( "\t", ("NA") x ( $col_num2 - 1 ) );
+        }
+    }
+}
+
+for my $key ( keys %table1 ) {
+    print("$key\t$table1{$key}\n");
+}
+
+__END__
