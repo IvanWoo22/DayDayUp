f21177af1fd53765f546cf24b93803621cf1a930
Ivan <wuyifanwd@hotmail.com>
2022-04-26 10:47:12 +0800

    220426.

--- 220425/log.sh
+++ 220425/log.sh
@@ -8,10 +8,10 @@ for(i=a+1;i<=a+13;i++) printf \$i \"\\t\"; print \$(a+14)}' > raw_P{2}_merge.tsv
 
 parallel --xapply -j 7 "
 pigz -dc CpG_sites_merge.tsv.gz |
-awk -va={1} 'NR==1{for(i=1;i<=2;i++){printf \"\\t\" \$i};
-for(i=a;i<=NF;i+=14){printf \"\\t\" \$i}; print \"\"};
-NR>1{for(i=1;i<=3;i++) printf \$i \"\\t\";
-for(i=a+1;i<=NF;i+=14) printf \$i \"\\t\"; print \$(a+14)}' > raw_L{2}_merge.tsv
+awk -va={1} 'NR==1{printf \"\\t\" \$1 \"\\t\" \$2;
+for(i=a;i<=NF;i+=14) printf \"\\t\" \$i \"\\t\" \$(i+1); print \"\"};
+NR>1{printf \$1 \"\\t\" \$2 \"\\t\" \$3;
+for(i=a+1;i<=NF;i+=14) printf \"\\t\" \$i \"\\t\" \$(i+1); print \"\"}' > raw_L{2}_merge.tsv
 " ::: {3..15..2} ::: {1..7}
 
 parallel --keep-order --xapply -j 15 "
@@ -25,11 +25,17 @@ pigz -dc CpG_sites_merge.tsv.gz |
     {a[sum]++}};END{for(i=1;i<=105;i++){print a[i]}}' >each_sample.tsv
 
 for P in {1..15}; do
-  parallel --xapply --keep-order -j 7 "awk -vk={} '{sum=0;{for(i=5;i<=17;i=i+2){if(\$i!=\"NA\"&&\$i>=15){sum++}}};{if(sum==k){print}}}' raw_P${P}_merge.tsv | wc -l" ::: {7..1}
+  parallel --xapply --keep-order -j 7 "
+    awk -vk={} '{sum=0;{for(i=5;i<=17;i=i+2){if(\$i!=\"NA\"&&\$i>=15){sum++}}};
+    {if(sum==k){print}}}' raw_P${P}_merge.tsv | wc -l
+    " ::: {7..1}
 done >each_loc_each_sample.tsv
 
 for L in {1..7}; do
-  parallel --xapply --keep-order -j 15 "awk -vk={} '{sum=0;{for(i=5;i<=NF;i=i+2){if(\$i!=\"NA\"&&\$i>=15){sum++}}};{if(sum==k){print}}}' raw_L${L}_merge.tsv | wc -l" ::: {15..1}
+  parallel --xapply --keep-order -j 15 "
+    awk -vk={} '{sum=0;{for(i=5;i<=NF;i=i+2){if(\$i!=\"NA\"&&\$i>=15){sum++}}};
+    {if(sum==k){print}}}' raw_L${L}_merge.tsv | wc -l
+    " ::: {15..1}
 done >each_sample_each_loc.tsv
 
 pigz -dc CpG_sites_merge.tsv.gz |
