4809e4a70f91d58d9c653f7c6fb2192d12de53b3
Ivan <wuyifanwd@hotmail.com>
2023-02-15 21:41:20 +0800

    230215.

---
+++ 230215/unibootstrap.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+BS_DIR=$1
+RESULT_FILE=$2
+TARGET=$3
+MIN=$4
+MAX=$5
+OUTPUT_DIR=${6:-.}
+
+if [ ! -d "${BS_DIR}" ]; then
+  echo "[${BS_DIR}] is not a dir" 1>&2
+  exit
+fi
+
+if [ ! -e "${RESULT_FILE}" ]; then
+  echo "[${RESULT_FILE}] is not a file" 1>&2
+  exit
+fi
+
+BASENAME=$(basename "${RESULT_FILE}")
+parallel --no-run-if-empty --linebuffer -k -j 24 "
+  echo '==> Bootstrap #{}' 1>&2
+  Rscript univalidate_Twist.R ${TARGET} ${BS_DIR}/data.tsv.{} ${RESULT_FILE}.{}
+  keep-header -- awk '\$6>${MIN}||\$6<${MAX}' \
+    <${RESULT_FILE}.{} |
+    cut -f 1 \
+      >${OUTPUT_DIR}/${BASENAME}.{}
+    " ::: "$(printf "%03d " {0..99})"
+
+echo '==> Outputs' 1>&2
+parallel --no-run-if-empty --linebuffer -k -j 100 "
+    cat ${OUTPUT_DIR}/${BASENAME}.{}
+    " ::: "$(printf "%03d " {0..99})" |
+  grep -v "^marker" |
+  sort --buffer-size=2G |
+  tsv-summarize --group-by 1 --count |
+  sort -k2,2nr --buffer-size=2G |
+  (echo -e "ProbeID\tBS" && cat) \
+    >"${OUTPUT_DIR}"/"${BASENAME}".count.tsv
+
+tsv-join \
+  --filter-file "${OUTPUT_DIR}"/"${BASENAME}".count.tsv \
+  --key-fields 1 --append-fields 2 \
+  <"${RESULT_FILE}" \
+  >"${OUTPUT_DIR}"/"${BASENAME}".bootstrap.tsv
---
+++ 230215/univalidate_Twist.R
@@ -0,0 +1,84 @@
+#!/usr/bin/env Rscript
+library(pROC, quietly = T)
+library(data.table, quietly = T)
+library(readr, quietly = T)
+
+Args <- commandArgs(T)
+target <- Args[1]
+data_path <- Args[2]
+output_path <- Args[3]
+
+do_logistic_ML <- function(df_test, ProbeID) {
+  formula_string <- paste("judge ~ ", ProbeID, sep = "")
+  res_logistic <-
+    glm(as.formula(formula_string), df_test, family = "binomial")
+  
+  #------------------------------------------------------#
+  # ROC
+  #------------------------------------------------------#
+  if (length(df_test$judge) == length(res_logistic$fitted.values)) {
+    rocauc <- format(round(
+      roc(
+        df_test$judge,
+        res_logistic$fitted.values,
+        levels = c(F, T),
+        direction = "<"
+      )$auc,
+      5
+    ), nsmall = 5)
+  } else {
+    rocauc <- NA
+  }
+  
+  return(as.data.frame(
+    cbind(
+      ProbeID,
+      format(round(
+        summary(res_logistic)$coefficients[2, 1], 5
+      ), nsmall = 5),
+      format(round(
+        summary(res_logistic)$coefficients[2, 4], 5
+      ), nsmall = 5),
+      format(round(((summary(res_logistic)$null.deviance / -2) - (summary(res_logistic)$deviance / -2)
+      ) / (
+        summary(res_logistic)$null.deviance / -2
+      ), 5), nsmall = 5),
+      format(round(1 - pchisq(2 * (
+        (summary(res_logistic)$deviance / -2) - (summary(res_logistic)$null.deviance / -2)
+      ), df = 1), 10), nsmall = 10),
+      rocauc
+    ),
+    stringAsFactor = F
+  ))
+}
+
+test_df <-
+  read.csv(data_path,
+           header = T,
+           sep = "\t")
+
+test_df$judge <- F
+test_df[test_df$status == target, ]$judge <- T
+test_df$judge <- as.factor(test_df$judge)
+
+write_delim(
+  data.frame(t(
+    c("#marker", "coef", "coef_p", "R_2", "reg_p", "rocauc")
+  )),
+  output_path,
+  append = TRUE,
+  col_names = F,
+  delim = "\t"
+)
+
+for (ProbeID in colnames(test_df)[4:ncol(test_df) - 1]) {
+  output <-
+    do_logistic_ML(test_df[, which(names(test_df) %in% c(ProbeID, "judge"))], ProbeID)
+  write_delim(
+    output,
+    output_path,
+    append = TRUE,
+    col_names = F,
+    delim = "\t"
+  )
+}
