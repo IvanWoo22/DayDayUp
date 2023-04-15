859606beed29b2e141c520597ce8bfc9d23dd93f
Ivan <wuyifanwd@hotmail.com>
2023-04-15 12:11:19 +0800

    230414.

--- 230405/miRNA.sh
+++ 230405/miRNA.sh
@@ -6,6 +6,7 @@ for i in NJU1013 NJU1014; do
     -o ${i}/R1.fq.gz -p ${i}/R2.fq.gz \
     >${i}/cutadapt.log 2>&1
   pear -j 4 -f ${i}/R1.fq.gz -r ${i}/R2.fq.gz -o ${i} -n 10
-  pigz -dc ${i}.assembled.fastq.gz | perl ../perbool/fastq2count.pl >${i}/reads.count
+  perl ../../perbool/fastq2count.pl <${i}.assembled.fastq >${i}/reads.count
   perl mirna_count.pl ${i}/reads.count hsa_mirna.fa >${i}/mir.count.tsv
 done
+
