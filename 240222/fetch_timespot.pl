4bb27e225562a582ac76a6ed04717f3ed2cedef8
Ivan <wuyifanwd@hotmail.com>
2024-02-26 12:35:55 +0800

    240225.

--- 240222/fetch_timespot.pl
+++ 240222/fetch_timespot.pl
@@ -1,3 +1,25 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
-use warnings FATAL => 'all';
+use warnings;
+
+my %feature;
+open( my $TIME, "<", $ARGV[0] );
+while (<$TIME>) {
+    chomp;
+    my @tmp = split "\t";
+    $feature{ $tmp[0] } = 1;
+}
+close($TIME);
+
+open( my $LINE, "<", $ARGV[1] );
+while (<$LINE>) {
+    chomp;
+    my @tmp  = split "\t";
+    my $time = $tmp[2] . $tmp[3] . $tmp[4] . $tmp[5] . $tmp[6] . $tmp[7];
+    if ( exists( $feature{$time} ) ) {
+        print "$tmp[0]\t$tmp[1]\n";
+    }
+}
+close($LINE);
+
+__END__
--- 240222/matrix_generation.sh
+++ 240222/matrix_generation.sh
@@ -1,10 +1,17 @@
-cut -f 3,4,5,6,7,8 Atha_links_timespot.tsv | sort | uniq \
-	| awk '{
+perl split_lines.pl ../revis/RMasked/Results/Atha/Atha.links.tsv Atha_links2.txt
+grep -v "Mt" Atha_links2.txt >Atha_links2.tsv
+
+perl ../data/classify_links_neo.pl Atha_links2.tsv \
+	Atha_Asue.yml Atha_Alyr.yml Atha_Risl.yml Atha_Esyr.yml Atha_Cvio.yml Atha_Ghir.yml \
+	0.5 >Atha_links2_timespot.tsv
+
+cut -f 3,4,5,6,7,8 Atha_links2_timespot.tsv | sort | uniq -c \
+	| awk -va=1 '{
     count0 = 0;
     count1 = 0;
     count2 = 0;
     count3 = 0;
-    for (i = a; i <= NF; i++) {
+    for (i = (a+1); i <= NF; i++) {
         if ($i == 0) {
             count0++;
         } else if ($i == 1) {
@@ -18,4 +25,40 @@ cut -f 3,4,5,6,7,8 Atha_links_timespot.tsv | sort | uniq \
     if ((count0 >= 0 && count1 >= 0 && count2 == 0 && count3 == 0) || (count0 >= 0 && count1 == 0 && count2 >= 0 && count3 == 0)) {
         print;
     }
-}'
+}' | awk '{print $2 $3 $4 $5 $6 $7 "\t" $1}' >time1.txt
+
+for i in {2..7}; do
+	cut -f 3,4,5,6,7,8 Atha_links2_timespot.tsv | sort | uniq -c \
+		| awk -va="$i" '{
+    count0 = 0;
+    count1 = 0;
+    count2 = 0;
+    count3 = 0;
+    for (i = (a+1); i <= NF; i++) {
+        if ($i == 0) {
+            count0++;
+        } else if ($i == 1) {
+            count1++;
+        } else if ($i == 2) {
+            count2++;
+        } else if ($i == 3) {
+            count3++;
+        }
+    }
+    if ((count0 >= 0 && count1 >= 0 && count2 == 0 && count3 == 0) || (count0 >= 0 && count1 == 0 && count2 >= 0 && count3 == 0)) {
+        print;
+    }
+}' | awk -va="$i" '$a==3{print $2 $3 $4 $5 $6 $7 "\t" $1}' >time"${i}".txt
+done
+
+for i in {1..7}; do
+	perl fetch_timespot.pl \
+		time"${i}".txt Atha_links2_timespot.tsv >Atha_links2_timespot_"${i}".tsv
+done
+
+for i in {1..7}; do
+	perl ../promotor_intsec.pl \
+		../Test0110/AT.promoter.bed Atha_links2_timespot_"${i}".tsv >Atha_links2_timespot_"${i}"_prom.tsv
+	perl ../data/arg_meth_link.pl \
+		../Memory/AT.beta.1.tsv Atha_links2_timespot_"${i}"_prom.tsv Atha_links2_timespot_"${i}"_prom_beta.tsv 3
+done
